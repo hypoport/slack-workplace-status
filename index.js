848528b75f83b477ba58c32529b4885ea45387dd
Susanne Grell <susanne.grell@europace.de>
2016-12-12 15:30:14 +0100

    logging

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ var TOKEN = process.env.SLACK_TOKEN
 // Slack slash command verify token
 var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN
 
+console.log("token: "+ TOKEN)
+console.log("verify token: "+VERIFY_TOKEN)
 var controller = botkit.slackbot()
 var bot = controller.spawn({ token: TOKEN }).startRTM()
 

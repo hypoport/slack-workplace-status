6799dfecca528a89b29850ad4437512ca01ccebe
Susanne Grell <susanne.grell@europace.de>
2016-12-12 14:35:19 +0100

    hello world

--- index.js
+++ index.js
@@ -23,8 +23,10 @@
  Read all about it here:
  -> http://howdy.ai/botkit
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var express = require('express');
+var app = express();
 
-var Botkit = require('botkit');
+//var Botkit = require('botkit');
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
@@ -43,56 +45,65 @@ var config = {}
     };
 }*/
 
-var controller = Botkit.slackbot(config).configureSlackApp(
-    {
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        scopes: ['commands'],
-    }
-);
+// var controller = Botkit.slackbot(config).configureSlackApp(
+//     {
+//         clientId: process.env.CLIENT_ID,
+//         clientSecret: process.env.CLIENT_SECRET,
+//         scopes: ['commands'],
+//     }
+// );
 
-controller.setupWebserver(process.env.PORT, function (err, webserver) {
-    controller.createWebhookEndpoints(controller.webserver);
+// controller.setupWebserver(process.env.PORT, function (err, webserver) {
+//     controller.createWebhookEndpoints(controller.webserver);
 
-    //controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
-    //    if (err) {
-    //        res.status(500).send('ERROR: ' + err);
-    //    } else {
-    //        res.send('Success!');
-    //    }
-    //});
-});
+//     //controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+//     //    if (err) {
+//     //        res.status(500).send('ERROR: ' + err);
+//     //    } else {
+//     //        res.send('Success!');
+//     //    }
+//     //});
+// });
 
 
 //
 // BEGIN EDITING HERE!
 //
 
-controller.on('slash_command', function (slashCommand, message) {
+// controller.on('slash_command', function (slashCommand, message) {
 
-    switch (message.command) {
-        case "/wps": 
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+//     switch (message.command) {
+//         case "/wps": 
+//             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
 
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
-                return;
-            }
+//             // if no text was supplied, treat it as a help command
+//             if (message.text === "" || message.text === "help") {
+//                 slashCommand.replyPrivate(message,
+//                     "I echo back what you tell me. " +
+//                     "Try typing `/echo hello` to see.");
+//                 return;
+//             }
 
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
+//             // If we made it here, just echo what the user typed back at them
+//             //TODO You do it!
+//             slashCommand.replyPublic(message, "1", function() {
+//                 slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//             });
 
-            break;
-        default:
-            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+//             break;
+//         default:
+//             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
 
-    }
+//     }
 
-})
-;
+// });
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port '+process.env.PORT);
+});
